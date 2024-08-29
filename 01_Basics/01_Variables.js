657014bf38d33ed98224567daef0d9d73bd17cfb
kinza-sheikh <kinzaubaidullah74@gmail.com>
2024-08-29 11:28:46 +0000

    changes done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 144500
+let accountEmail ="kinzasheikh403@gmail.com"
+var accountPassword ="12345"
+let accountState;
+accountCity = "phool nagar"
+
+// accountId = 2 //not allowed
+accountEmail = "h.hc@.com"
+accountPassword = "212121"
+accountCity ="pattoki"
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState])
+
+// not prefer to use var because scope issue so let used in js programmming
