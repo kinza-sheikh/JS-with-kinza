28844885dd122a7f2dd8a207868ba40d241e19bb
kinza-sheikh <kinzaubaidullah74@gmail.com>
2024-08-31 06:56:25 +0000

    datatype

--- 01_Basics/02_datatype.js
+++ 01_Basics/02_datatype.js
@@ -1,3 +1,27 @@
 "use strict" // treat all js code as newer version
 
 // alert(3 + 3) //we are using nodejs,not browswer
+
+
+
+let name = "kinza"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// PREMITIVE DATA TYPE
+
+//number => 2 to power 53
+//bigint =>for higher values
+// string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//Symbol => for uniqness
+
+
+//OBJECT
+console.log(typeof age);
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // object
