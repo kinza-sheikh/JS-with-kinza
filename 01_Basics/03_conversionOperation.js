a5b631acb1b5f898ed61261b99e7f1b82354b8eb
kinza-sheikh <kinzaubaidullah74@gmail.com>
2024-08-31 08:41:33 +0000

    done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+//let score = null
+//let score = "33" =>33
+//let score = "33abc" =>Nan
+//let score = undefined
+//let score = "kinza"
+//true =>1 , false =>0
+
+// two methods
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = 5
+
+let booleanIsLogged = Boolean(isLoggedIn)
+console.log(booleanIsLogged);
+
+//1 =>true, 0 =>false
+//""=> false
+// "name" => true
+
+
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
