be278a3f24fe7f7f6cbd74a594679ec7d0c547d5
kinza-sheikh <kinzaubaidullah74@gmail.com>
2024-09-04 10:34:33 +0000

    datatypes-summary

---
+++ 01_Basics/05_datatypes-summary.js
@@ -0,0 +1,46 @@
+// premitive
+
+// 7 types  : string, number, symbol, boolean, null, undefined, BigInt
+
+
+
+
+// const score = 100
+// const scoreValue = 100.67
+
+// const isLoggedIn = false
+const outsideTemp = null
+
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+
+const bigNumber = 765556267812681672n
+
+// Refrence type (Non-premitive)
+
+// Arrays, functions, objects
+
+// arrays exampl ::
+// const heros =["bilal", "kinza", "nimrta"];
+
+// obj exampl
+
+// let myObj ={
+//     name : "kinza"
+//     age: 12,
+// }
+
+// // function exampl
+// const myFunction = function() {
+//     console.log("hello");
+// }
+
+
+// datatype maloom krny ka syntax
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
